cc2e60c5267afd17b5ab285be94adbeab53748ce
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2025-11-01 16:08:47 +0530

    creating assets file saprete the import code and adding loading lazy for enhance the image

---
+++ client/src/assets/assets-portfolio.js
@@ -0,0 +1,170 @@
+// Posters images imports  
+import coverdesign from "./PortfolioImg/posters/coverdesign.png";
+import Celestial from "./PortfolioImg/posters/Celestial.webp";
+import CyberpunkMagazine from "./PortfolioImg/posters/CyberpunkMagazine.webp";
+import Dreadposter from "./PortfolioImg/posters/Dreadposter.webp";
+import FlighttoFreedomposter from "./PortfolioImg/posters/FlighttoFreedomposter.webp";
+import Gameposterdesign from "./PortfolioImg/posters/Gameposterdesign.webp";
+import gamingposter from "./PortfolioImg/posters/gamingposter.webp";
+import Geniusposterdesign from "./PortfolioImg/posters/Geniusposterdesign.webp";
+import lunarlegacybookcover from "./PortfolioImg/posters/lunarlegacybookcover.webp";
+import OceansWhisper from "./PortfolioImg/posters/Ocean_s Whisper.webp";
+import photoshopdesigns from "./PortfolioImg/posters/photoshopdesigns.webp";
+import photoshopposterdesign from "./PortfolioImg/posters/photoshopposterdesign.webp";
+import posterdesigns from "./PortfolioImg/posters/posterdesigns.webp";
+
+// CGI images imports
+import cgi1 from "./PortfolioImg/CGI/1.png";
+import cgi2 from "./PortfolioImg/CGI/2.png";
+import cgi4 from "./PortfolioImg/CGI/4.png";
+import designAds3d from "./PortfolioImg/CGI/3ddesignads.webp";
+import whiskyModel from "./PortfolioImg/CGI/3dmodellingwhisky.webp";
+import jewelryDesign3d from "./PortfolioImg/CGI/3djewelrydesign.webp";
+import jewelryDesigns3d from "./PortfolioImg/CGI/3djewelrydesigns.webp";
+import modeling3d from "./PortfolioImg/CGI/3dmodeling.webp";
+import perfumeDesign3d from "./PortfolioImg/CGI/3dperfumedesign.webp";
+import perfumeDesigns3d from "./PortfolioImg/CGI/perfumedesigns3d.webp";
+import upvcWindowDesigns3d from "./PortfolioImg/CGI/upvcwindow3ddesiogns.webp";
+import upvcWindowAds3ds from "./PortfolioImg/CGI/upvcwindowads3ds.webp";
+
+// Stills images imports
+import addesigns from "./PortfolioImg/Stills/addesigns.webp";
+import creativephotomanipulation from "./PortfolioImg/Stills/creativephotomanipulation.webp";
+import creativephotoshopedit from "./PortfolioImg/Stills/creativephotoshopedit.webp";
+import doctorsdaycreativedesign from "./PortfolioImg/Stills/doctorsdaycreativedesign.webp";
+import doctorsdayphotoshopedit from "./PortfolioImg/Stills/doctorsdayphotoshopedit.webp";
+import freepikEnhance85525 from "./PortfolioImg/Stills/freepik__enhance__85525.jpeg";
+import ganeshaphotoedit from "./PortfolioImg/Stills/ganeshaphotoedit.webp";
+import ikeaaddesigns from "./PortfolioImg/Stills/ikeaaddesigns.webp";
+import KFCaddesigns from "./PortfolioImg/Stills/KFCaddesigns.webp";
+import krishnajanmashtmidesign from "./PortfolioImg/Stills/krishnajanmashtmidesign.webp";
+import malariadayphotoshopedit from "./PortfolioImg/Stills/malariadayphotoshopedit.webp";
+import mcdonaladsdesigns from "./PortfolioImg/Stills/mcdonaladsdesigns.webp";
+import netflixaddesigns from "./PortfolioImg/Stills/netflixaddesigns.webp";
+import photoshopdesigndurex from "./PortfolioImg/Stills/photoshopdesigndurex.webp";
+import photoshopedits from "./PortfolioImg/Stills/photoshopedits.webp";
+import redbullsleepdaydesign from "./PortfolioImg/Stills/redbullsleepdaydesign.webp";
+import uniquedesigns from "./PortfolioImg/Stills/uniquedesigns.webp";
+import upvcdoorcreativedesigns from "./PortfolioImg/Stills/upvcdoorcreativedesigns.webp";
+import upvcwindowcreativeads from "./PortfolioImg/Stills/upvcwindowcreativeads.webp";
+import worldmalariaphotoshop from "./PortfolioImg/Stills/worldmalariaphotoshop.webp";
+
+// Brand Guidelines images imports
+import cv01 from "./PortfolioImg/brandguidelines/CV-01.webp";
+import cv02 from "./PortfolioImg/brandguidelines/CV-02.webp";
+import cv03 from "./PortfolioImg/brandguidelines/CV-03.webp";
+import cv04 from "./PortfolioImg/brandguidelines/CV-04.webp";
+import cv05 from "./PortfolioImg/brandguidelines/CV-05.webp";
+import cv06 from "./PortfolioImg/brandguidelines/CV-06.webp";
+import rising01 from "./PortfolioImg/brandguidelines/rising-01.webp";
+import rising02 from "./PortfolioImg/brandguidelines/rising-02.webp";
+import rising03 from "./PortfolioImg/brandguidelines/rising-03.webp";
+import rising04 from "./PortfolioImg/brandguidelines/rising-04.webp";
+import rising05 from "./PortfolioImg/brandguidelines/rising-05.webp";
+import ws01 from "./PortfolioImg/brandguidelines/WS-01.webp";
+import ws02 from "./PortfolioImg/brandguidelines/WS-02.webp";
+import ws03 from "./PortfolioImg/brandguidelines/WS-03.webp";
+import ws04 from "./PortfolioImg/brandguidelines/WS-04.webp";
+import ws05 from "./PortfolioImg/brandguidelines/WS-05.webp";
+import ws06 from "./PortfolioImg/brandguidelines/WS-06.webp";
+import ws07 from "./PortfolioImg/brandguidelines/WS-07.webp";
+import dwed01 from "./PortfolioImg/brandguidelines/dwed-01.jpg";
+
+// Brochure images imports
+import eure01 from "./PortfolioImg/brochure/eure-01.webp";
+import eure02 from "./PortfolioImg/brochure/eure-02.webp";
+import eure03 from "./PortfolioImg/brochure/eure-03.webp";
+import eure04 from "./PortfolioImg/brochure/eure-04.webp";
+import eure05 from "./PortfolioImg/brochure/eure-05.webp";
+import upvc01 from "./PortfolioImg/brochure/upvcwindowsbrochuredesign-01.webp";
+import upvc02 from "./PortfolioImg/brochure/upvcwindowsbrochuredesign-02.webp";
+import upvc03 from "./PortfolioImg/brochure/upvcwindowsbrochuredesign-03.webp";
+import upvc04 from "./PortfolioImg/brochure/upvcwindowsbrochuredesign-04.webp";
+
+export const PostersPortData = [
+    coverdesign,
+    Celestial,
+    CyberpunkMagazine,
+    Dreadposter,
+    FlighttoFreedomposter,
+    Gameposterdesign,
+    gamingposter,
+    Geniusposterdesign,
+    lunarlegacybookcover,
+    OceansWhisper,
+    photoshopdesigns,
+    photoshopposterdesign,
+    posterdesigns,
+]
+
+export const CGIPortData = [
+    cgi1,
+    cgi2,
+    cgi4,
+    designAds3d,
+    whiskyModel,
+    jewelryDesign3d,
+    jewelryDesigns3d,
+    modeling3d,
+    perfumeDesign3d,
+    perfumeDesigns3d,
+    upvcWindowDesigns3d,
+    upvcWindowAds3ds,
+]
+
+export const StillsPortData = [
+    addesigns,
+    creativephotomanipulation,
+    creativephotoshopedit,
+    doctorsdaycreativedesign,
+    doctorsdayphotoshopedit,
+    freepikEnhance85525,
+    ganeshaphotoedit,
+    ikeaaddesigns,
+    KFCaddesigns,
+    krishnajanmashtmidesign,
+    malariadayphotoshopedit,
+    mcdonaladsdesigns,
+    netflixaddesigns,
+    photoshopdesigndurex,
+    photoshopedits,
+    redbullsleepdaydesign,
+    uniquedesigns,
+    upvcdoorcreativedesigns,
+    upvcwindowcreativeads,
+    worldmalariaphotoshop,
+]
+
+export const BrandGuidelinesPortData = [
+    cv01,
+    cv02,
+    cv03,
+    cv04,
+    cv05,
+    cv06,
+    rising01,
+    rising02,
+    rising03,
+    rising04,
+    rising05,
+    ws01,
+    ws02,
+    ws03,
+    ws04,
+    ws05,
+    ws06,
+    ws07,
+    dwed01,
+]
+
+export const BrochurePortData = [
+    eure01,
+    eure02,
+    eure03,
+    eure04,
+    eure05,
+    upvc01,
+    upvc02,
+    upvc03,
+    upvc04,
+]
